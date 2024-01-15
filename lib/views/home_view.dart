d765701a70141084b0d7810441349764045dbf0b
Your Name <kareem01229526319@gmail.com>
2024-01-15 20:40:53 +0200

    finishing the appbar
    Signed-off-by: Your Name <kareem01229526319@gmail.com>

---
+++ lib/views/home_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(
+                color: Colors.orange,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
