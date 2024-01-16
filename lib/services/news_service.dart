aafc176da31f44fa96d97302a6513f352e649079
Your Name <kareem01229526319@gmail.com>
2024-01-16 21:10:52 +0200

    finish news services
    Signed-off-by: Your Name <kareem01229526319@gmail.com>

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,15 +1,27 @@
 import 'package:dio/dio.dart';
+import 'package:newsapp/models/article_model.dart';
 
 class NewsServices {
   final Dio dio;
   NewsServices(this.dio);
 
-  getNews() async {
+  Future<List<ArticleModel>> getNews() async {
     // String url = '';
     Response response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?country=eg&apiKey=3c88955c487e4d9db668f011dd85e737&category=general');
+        'https://newsapi.org/v2/top-headlines?country=us&apiKey=3c88955c487e4d9db668f011dd85e737&category=general');
     Map<String, dynamic> jsonData = response.data;
     List<dynamic> articls = jsonData['articles'];
-    print(articls);
+    // print(articls);
+    List<ArticleModel> articlesList = [];
+    for (var article in articls) {
+      ArticleModel articleModel = ArticleModel(
+        image: article['urlToImage'],
+        title: article['title'],
+        subTitle: article['description'],
+      );
+      articlesList.add(articleModel);
+    }
+    // print(articlesList);
+    return articlesList;
   }
 }
